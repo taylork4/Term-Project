2a88c60c78cc09236b5ada57b58d8a2b66f7e203
Kyle Taylor <taylork4@mail.gvsu.edu>
2022-01-30 15:22:49 -0500

    Added in variables for the card deck, the discard pile, and whether the game is supposed to keep going

--- Rummy.java
+++ Rummy.java
@@ -1,1 +1,17 @@
-//File Add in
+import java.util.*;
+public class Rummy {
+
+    private int[] deck;
+    private int[] discard;
+    private boolean gameInProg;
+    //private Player player;
+
+    public Rummy() {
+        deck = new int[51];
+        discard = new int[51];
+        gameInProg = true;
+    }
+    public static void main(String[] args) {
+        System.out.println("Getting progress");
+    }
+}
