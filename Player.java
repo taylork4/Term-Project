a0ddcf547361c2177b5c16c70cd718b5075d1fc6
Kyle Taylor <taylork4@mail.gvsu.edu>
2022-02-26 14:39:21 -0500

    Added commenting to the Player class

--- Player.java
+++ Player.java
@@ -1,17 +1,42 @@
 public class Player {
-    private int score;
-    public Deck hand;
+    /**************************************************************************************************************************************************************************************
+     * 
+     * The Player class sets the score and creates a hand where the players cards
+     * will go into at the start
+     * of the game.
+     * 
+     * @author Noah Arden
+     * @author Owen Carroll
+     * @author Kyle Taylor
+     * @version 2/28/2022
+     * 
+     */
 
-    public Player(){
-        score = 0;          //player starts with empty deck and no score. 
-        hand = new Deck(0); //Cards will be dealt at the start of the game (from Rummy class)
+    /**************************************************************************************************************************************************************************************
+     * 
+     * Instance Variables
+     * 
+     */
+    private int score;
+    private Deck hand;
+
+    /**************************************************************************************************************************************************************************************
+     * 
+     * Default constructor of the Card class.
+     * 
+     */
+    public Player() {
+        score = 0; // Player starts with empty deck and no score.
+        hand = new Deck(0); // Cards will be dealt at the start of the game (from Rummy class)
     }
 
-    public void addPoint(){
+    /**************************************************************************************************************************************************************************************
+     * 
+     * The addPoint() method awards points to the player.
+     * 
+     */
+    public void addPoint() {
         this.score++;
     }
 
-     
-
 }
-
